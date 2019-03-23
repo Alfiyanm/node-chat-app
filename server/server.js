c412f533642548b75fba3d0738971b08b5a7e2db
Muchlas Alfiyan <muchlas12@gmail.com>
2019-03-23 17:08:18 +0700

    first

---
+++ server/server.js
@@ -0,0 +1,30 @@
+const path = require('path');
+const http = require('http');
+const express = require('express');
+const socketIO = require('socket.io');
+
+const publicPath = path.join(__dirname, '../public');
+const port = process.env.PORT || 3000;
+var app = express();
+var server = http.createServer(app);
+var io = socketIO(server);
+
+app.use(express.static(publicPath));
+
+io.on('connection', (socket) => {
+  console.log('New user connected');
+
+  socket.emit('newEmail', {
+    from: 'muchlas12@gmail.com',
+    text: 'Hey, What is going on',
+    createAt: 123
+  });
+
+  socket.on('disconnect', () => {
+    console.log('User was disconnected');
+  });
+});
+
+server.listen(port, () => {
+  console.log(`Server is up on ${port}`);
+});
